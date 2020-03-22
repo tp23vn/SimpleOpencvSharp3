9bb8872b1248bfc9a7ece5ccfbaf24e0b533e3c6
tp23vn <taiphungdinh@gmail.com>
2020-03-22 22:57:20 +0700

    Initial project
    					2020/03/22
    
    DESCRIPTION
    
      A simple project with function to show image and convert image to
      gray. Application can run successfully:
      - Open image
      - Zoom in/Zoom out
      - Auto scroll
      - Convert image to gray using OpencvSharp3 library

---
+++ ImageViewer/PhotoViewer.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace ImageViewer
+{
+    partial class PhotoViewer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PhotoViewer));
+            this.mnsMain = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pnlViewer = new System.Windows.Forms.Panel();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnOpen = new System.Windows.Forms.ToolStripButton();
+            this.btnZoomIn = new System.Windows.Forms.ToolStripButton();
+            this.btnZoomOut = new System.Windows.Forms.ToolStripButton();
+            this.btnGrayConversion = new System.Windows.Forms.ToolStripButton();
+            this.ptbViewer = new System.Windows.Forms.PictureBox();
+            this.mnsMain.SuspendLayout();
+            this.pnlViewer.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbViewer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // mnsMain
+            // 
+            this.mnsMain.BackColor = System.Drawing.SystemColors.Control;
+            this.mnsMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.mnsMain.Location = new System.Drawing.Point(0, 0);
+            this.mnsMain.Name = "mnsMain";
+            this.mnsMain.Size = new System.Drawing.Size(520, 24);
+            this.mnsMain.TabIndex = 3;
+            this.mnsMain.Text = "Main Menu";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // pnlViewer
+            // 
+            this.pnlViewer.AutoScroll = true;
+            this.pnlViewer.AutoSize = true;
+            this.pnlViewer.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.pnlViewer.Controls.Add(this.ptbViewer);
+            this.pnlViewer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlViewer.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.pnlViewer.Location = new System.Drawing.Point(0, 24);
+            this.pnlViewer.Name = "pnlViewer";
+            this.pnlViewer.Size = new System.Drawing.Size(520, 496);
+            this.pnlViewer.TabIndex = 4;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnOpen,
+            this.btnZoomIn,
+            this.btnZoomOut,
+            this.btnGrayConversion});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 24);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(520, 25);
+            this.toolStrip1.TabIndex = 5;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnOpen.Image = global::ImageViewer.Properties.Resources.open_file_icon;
+            this.btnOpen.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(23, 22);
+            this.btnOpen.Text = "Open";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnZoomIn.Enabled = false;
+            this.btnZoomIn.Image = global::ImageViewer.Properties.Resources.zoom_in;
+            this.btnZoomIn.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(23, 22);
+            this.btnZoomIn.Text = "Zoom In";
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnZoomOut.Enabled = false;
+            this.btnZoomOut.Image = global::ImageViewer.Properties.Resources.zoom_out;
+            this.btnZoomOut.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(23, 22);
+            this.btnZoomOut.Text = "Zoom Out";
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // btnGrayConversion
+            // 
+            this.btnGrayConversion.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.btnGrayConversion.Enabled = false;
+            this.btnGrayConversion.Image = global::ImageViewer.Properties.Resources.gray_conversion;
+            this.btnGrayConversion.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnGrayConversion.Name = "btnGrayConversion";
+            this.btnGrayConversion.Size = new System.Drawing.Size(23, 22);
+            this.btnGrayConversion.Text = "Convert To Gray";
+            this.btnGrayConversion.Click += new System.EventHandler(this.btnGrayConversion_Click);
+            // 
+            // ptbViewer
+            // 
+            this.ptbViewer.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ptbViewer.Location = new System.Drawing.Point(2, 28);
+            this.ptbViewer.Name = "ptbViewer";
+            this.ptbViewer.Size = new System.Drawing.Size(515, 464);
+            this.ptbViewer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.ptbViewer.TabIndex = 0;
+            this.ptbViewer.TabStop = false;
+            // 
+            // PhotoViewer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(520, 520);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.pnlViewer);
+            this.Controls.Add(this.mnsMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "PhotoViewer";
+            this.Text = "Simple Photo Viewer";
+            this.Load += new System.EventHandler(this.PhotoViewer_Load);
+            this.mnsMain.ResumeLayout(false);
+            this.mnsMain.PerformLayout();
+            this.pnlViewer.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ptbViewer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnsMain;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.Panel pnlViewer;
+        private System.Windows.Forms.PictureBox ptbViewer;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnOpen;
+        private System.Windows.Forms.ToolStripButton btnZoomIn;
+        private System.Windows.Forms.ToolStripButton btnZoomOut;
+        private System.Windows.Forms.ToolStripButton btnGrayConversion;
+    }
+}
+
---
+++ ImageViewer/PhotoViewer.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenCvSharp;
+
+namespace ImageViewer
+{
+    public partial class PhotoViewer : Form
+    {
+        private double _scaleRatio;
+        private string _inputImagePath;
+        private bool _bGrayMode;
+        Image _originalImage;
+        Bitmap _grayImage;
+
+        public PhotoViewer()
+        {
+            _scaleRatio = 1.0;
+            _bGrayMode = false;
+            InitializeComponent();
+        }
+
+        private void PhotoViewer_Load(object sender, EventArgs e)
+        {
+            this.ptbViewer.MouseWheel += this.ptbViewer_MouseWheel;
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openImageFile();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void ptbViewer_MouseWheel(object sender, MouseEventArgs e)
+        {
+            if (_scaleRatio >= 0.125 && _scaleRatio <= 4)
+            {
+                if (e.Delta > 0)
+                {
+                    zoomIn();
+                }
+                else
+                {
+                    zoomOut();
+                }
+            }
+        }
+
+        private void doZoom() {
+            ptbViewer.Width = (int)(ptbViewer.Image.Width * _scaleRatio);
+            ptbViewer.Height = (int)(ptbViewer.Image.Height * _scaleRatio);
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string message = "Developed by Tai Phung\nEmail: taiphungdinh@gmail.com\nGithub: www.github.com/tp23vn";
+            MessageBox.Show(message, "About", MessageBoxButtons.OK, MessageBoxIcon.Information, MessageBoxDefaultButton.Button1);
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            openImageFile();
+        }
+
+        private void openImageFile() {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files (*.png, *.jpeg, *.jpg, *.bmp)|*.png;*.jpg;*.jpeg;*.bmp|PNG (*.png)|*.png|JPEG (*.jpeg)|*.jpeg|JPG (*.jpg)|*.jpg|BMP (*.bmp)|*.bmp";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                _originalImage = Image.FromFile(openFileDialog.FileName);
+                ptbViewer.Image = _originalImage;
+                ptbViewer.Width = ptbViewer.Image.Width;
+                ptbViewer.Height = ptbViewer.Image.Height;
+                _inputImagePath = openFileDialog.FileName;
+                _grayImage = null;
+                btnZoomIn.Enabled = true;
+                btnZoomOut.Enabled = true;
+                btnGrayConversion.Enabled = true;
+            }
+        }
+
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            zoomIn();
+        }
+
+        private void zoomIn()
+        {
+            if (_scaleRatio < 4)
+            {
+                _scaleRatio *= 2;
+                doZoom();
+            }
+        }
+
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            zoomOut();
+        }
+
+        private void zoomOut()
+        {
+            if (_scaleRatio > 0.125)
+            {
+                _scaleRatio /= 2;
+                doZoom();
+            }
+        }
+
+        private void btnGrayConversion_Click(object sender, EventArgs e)
+        {
+            if (_bGrayMode)
+            {
+                ptbViewer.Image = _originalImage;
+                _bGrayMode = false;
+                btnGrayConversion.ToolTipText = "Convert To Gray";
+            }
+            else
+            {
+                if (_grayImage == null)
+                {
+                    Mat grayImageMat = new Mat(_inputImagePath, ImreadModes.Grayscale);
+                    _grayImage = OpenCvSharp.Extensions.BitmapConverter.ToBitmap(grayImageMat);
+                }
+                ptbViewer.Image = _grayImage;
+                _bGrayMode = true;
+                btnGrayConversion.ToolTipText = "Show Original Image";
+            }
+        }
+    }
+}
